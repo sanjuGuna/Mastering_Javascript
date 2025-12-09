3cccdf186209862788f8be19e3f689b2efa62e03
sanjuGuna <sanjay27it@gmail.com>
2025-12-09 09:04:30 +0530

    js files dir change

--- FoodApp/Backend/index.js
+++ FoodApp/Backend/index.js
@@ -1,8 +1,12 @@
 const express=require('express')
 const app=express()
+require('dotenv').config(); //To config dotenv
 const mongoose=require('mongoose');
 const Food=require('./models/Food');
 const PORT=process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-mongoose.connect('mongodb+srv://myAtlasDBUser:sanjay1210@myatlasclusteredu.w2msv.mongodb.net/?appName=myAtlasClusterEDU')
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true});
+
